77e84bfc30dc083ced9544d77e342977528b1562
Will Acton <will@willacton.com>
2019-12-07 15:20:26 -0800

    init

---
+++ src/todo_mvc/components.clj
@@ -0,0 +1,14 @@
+(ns todo-mvc.components
+	(:require [helix.core :as hx]))
+
+(defmacro title [& args]
+	`(hx/$ Title ~@args))
+
+(defmacro app-footer [& args]
+	`(hx/$ AppFooter ~@args))
+
+(defmacro new-todo [& args]
+	`(hx/$ NewTodo ~@args))
+
+(defmacro todo-item [& args]
+	`(hx/$ TodoItem ~@args))
